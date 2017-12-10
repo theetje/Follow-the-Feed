b6519ef07e7c2ed8f9d0c416502dd5dfa5215c99
theetje <delange.thomas@gmail.com>
2017-12-10 18:43:59 +0100

    Model Article/Articles af en View controller die het zou moeten doen.....

---
+++ Follow the Feed/Model/ArticleItems.swift
@@ -0,0 +1,31 @@
+//
+//  ArticleItems.swift
+//  Follow the Feed
+//
+//  Created by Thomas De lange on 10-12-17.
+//  Copyright © 2017 Thomas De lange. All rights reserved.
+//
+
+import Foundation
+
+struct Article: Codable {
+    var id: String
+    var name: String
+    var description: String
+    var url: URL
+    var urlToImage: URL
+    var publishedAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case description
+        case url
+        case urlToImage
+        case publishedAt
+    }
+}
+
+struct Articles: Codable {
+    let articles: [Article]
+}
